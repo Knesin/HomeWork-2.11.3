5ed4bdfe60b59b45137c55345dc8aa545931fa28
Maksim Kl <GuruHacker@yandex.ru>
2023-03-20 23:11:24 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include "Leaver.h"
+
+int main()
+{
+    system("chcp 1251");
+    Leaver lvr;
+    std::string s;
+    std::cout << "Введите имя: ";
+    std::cin >> s;
+    std::cout << lvr.leave(s) << std::endl;
+}
